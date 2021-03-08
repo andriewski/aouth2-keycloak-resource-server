98dda21e7f2e75c67e9b14a683154d63f7109f5a
Mark <plambiroo@gmail.com>
2021-03-08 14:57:01 +0300

    init project

---
+++ src/main/java/by/mark/oauth2/Oauth2KeycloakExampleApplication.java
@@ -0,0 +1,13 @@
+package by.mark.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2KeycloakExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2KeycloakExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/mark/oauth2/Oauth2KeycloakExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mark.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2KeycloakExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
