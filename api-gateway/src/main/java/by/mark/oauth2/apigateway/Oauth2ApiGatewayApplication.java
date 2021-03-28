953d90ea8070fd7b22281641c50549ea068fb13b
Mark <plambiroo@gmail.com>
2021-03-28 14:34:51 +0300

    Initializing Spring Cloud Routing Server

---
+++ api-gateway/src/main/java/by/mark/oauth2/apigateway/Oauth2ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package by.mark.oauth2.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ api-gateway/src/test/java/by/mark/oauth2/apigateway/Oauth2ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mark.oauth2.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
