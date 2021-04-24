f85fcf251b08e73dc868c81941e5af5ad786cfbe
Mark <plambiroo@gmail.com>
2021-04-24 20:43:21 +0300

    Adding simple project with init implementation of keycloak SPI

---
+++ my-remote-user-storage-provider/src/main/java/by/mark/spi/CustomRemoteUserStorageProviderApplication.java
@@ -0,0 +1,13 @@
+package by.mark.spi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomRemoteUserStorageProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomRemoteUserStorageProviderApplication.class, args);
+	}
+
+}
---
+++ my-remote-user-storage-provider/src/test/java/by/mark/spi/CustomRemoteUserStorageProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package by.mark.spi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomRemoteUserStorageProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
